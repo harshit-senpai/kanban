b1dca63a220a37ac26fe16c24a83008f73efc60d
harshit Mishra <miananya2@gmail.com>
2024-08-15 20:30:15 +0530

    deleteBoard types schema handler

---
+++ actions/deleteBoard/index.ts
@@ -0,0 +1,41 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+
+import { InputType, ReturnType } from "./types";
+import { DeleteBoard } from "./schema";
+
+import { createSafeAction } from "@/lib/createSafeAction";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id } = data;
+  let board;
+
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete.",
+    };
+  }
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+
+export const deleteCount = createSafeAction(DeleteBoard, handler);
