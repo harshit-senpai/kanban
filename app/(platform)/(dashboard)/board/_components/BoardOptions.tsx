ee8a3e72b30b1195ac60dcdeb8688b7b08b46986
harshit Mishra <miananya2@gmail.com>
2024-08-15 19:59:45 +0530

    BoardOptions

---
+++ app/(platform)/(dashboard)/board/_components/BoardOptions.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { MoreHorizontal, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface BoardOptionsProps {
+  id: string;
+}
+
+export const BoardOptions = ({ id }: BoardOptionsProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size="icon" variant="transparent" className="h-auto w-auto p-2">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-semibold text-center text-neutral-600 pb-4">
+          Board Actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+          onClick={() => {}}
+        >
+          Delete Board
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
