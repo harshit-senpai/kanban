47404485cb6ccf8d36c52dc93fbb5f29d31c57a2
harshit Mishra <miananya2@gmail.com>
2024-08-15 18:58:19 +0530

    updateBoard types schema handler

---
+++ actions/updateBoard/index.ts
@@ -0,0 +1,46 @@
+"use server";
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+import { InputType, ReturnType } from "./types";
+import { updateBoard } from "./schema";
+
+import { createSafeAction } from "@/lib/createSafeAction";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, id } = data;
+  let board;
+
+  try {
+    board = await db.board.update({
+      where: {
+        orgId,
+        id,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update",
+    };
+  }
+
+  revalidatePath(`/board/${id}`);
+
+  return { data: board };
+};
+
+export const UpdateBoard = createSafeAction(updateBoard, handler);
---
+++ actions/updateBoard/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+import { Board } from "@prisma/client";
+
+import { ActionState } from "@/lib/createSafeAction";
+
+import { updateBoard } from "./schema";
+
+export type InputType = z.infer<typeof updateBoard>;
+export type ReturnType = ActionState<InputType, Board>;
