625b7c6de35ea3cd8836d10abb6788e1dd92fa5b
harshit Mishra <miananya2@gmail.com>
2024-08-15 19:09:19 +0530

    using update action

--- app/(platform)/(dashboard)/board/_components/BoardTitleForm.tsx
+++ app/(platform)/(dashboard)/board/_components/BoardTitleForm.tsx
@@ -4,9 +4,14 @@ import { ElementRef, useRef, useState } from "react";
 
 import { Board } from "@prisma/client";
 
+import { toast } from "sonner";
+
 import { Button } from "@/components/ui/button";
 
 import { FormInput } from "@/components/form/formInput";
+
+import { useAction } from "@/hooks/useAction";
+import { UpdateBoard } from "@/actions/updateBoard";
 
 interface BoardTitleFormProps {
   data: Board;
@@ -17,6 +22,18 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
   const inputRef = useRef<ElementRef<"input">>(null);
 
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>(data.title);
+
+  const { execute } = useAction(UpdateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   const enableEditing = () => {
     setIsEditing(true);
@@ -32,7 +49,11 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
-    console.log("submited", title);
+
+    execute({
+      title,
+      id: data.id,
+    });
   };
 
   const onBlur = () => {
@@ -50,7 +71,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
           onBlur={onBlur}
           id="title"
           ref={inputRef}
-          defaultValue={data.title}
+          defaultValue={title}
           className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
         />
       </form>
@@ -63,7 +84,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       className="font-bold text-lg h-auto w-auto p-1 px-2"
       variant="transparent"
     >
-      {data.title}
+      {title}
     </Button>
   );
 };
