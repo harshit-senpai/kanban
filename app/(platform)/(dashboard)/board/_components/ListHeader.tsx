a0b304ee4b4bad6d054df5a78ee9f5c6bb58ad26
harshit Mishra <miananya2@gmail.com>
2024-08-18 16:33:31 +0530

    ListHeader renaming form

--- app/(platform)/(dashboard)/board/_components/ListHeader.tsx
+++ app/(platform)/(dashboard)/board/_components/ListHeader.tsx
@@ -1,15 +1,61 @@
+"use client";
+
+import { FormInput } from "@/components/form/formInput";
 import { List } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
 
 interface ListHeaderProps {
   data: List;
 }
 
 export const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        {data.title}
-      </div>
+      {isEditing ? (
+        <form className="flex-1 px-[2px]">
+          <input hidden name="id" value={data.id} />
+          <input hidden name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            id="title"
+            onBlur={() => {}}
+            placeholder="Enter List Title..."
+            defaultValue={data.title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
     </div>
   );
 };
