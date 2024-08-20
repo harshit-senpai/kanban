fd7ed6fb83665c7a1e6bde3441fdf8241dcd6b1b
harshit Mishra <miananya2@gmail.com>
2024-08-20 13:20:09 +0530

    using deleteList Action

--- app/(platform)/(dashboard)/board/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/_components/ListOptions.tsx
@@ -1,3 +1,5 @@
+import { ElementRef, useRef } from "react";
+
 import { MoreHorizontal, X } from "lucide-react";
 
 import { List } from "@prisma/client";
@@ -11,7 +13,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+import { toast } from "sonner";
+
 import { FormSubmit } from "@/components/form/formSubmit";
+import { useAction } from "@/hooks/useAction";
+import { deleteList } from "@/actions/deleteList";
 
 interface ListOptionsProps {
   data: List;
@@ -19,6 +25,25 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" deleted`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({ id, boardId });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -32,6 +57,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         </div>
         <PopoverClose asChild>
           <Button
+            ref={closeRef}
             className="h-auto w-auto absolute top-2 right-2 text-neutral-600"
             variant="ghost"
           >
@@ -56,7 +82,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
           </FormSubmit>
         </form>
         <Separator />
-        <form>
+        <form action={onDelete}>
           <input name="id" value={data.id} hidden id="id" />
           <input value={data.boardId} name="boardId" hidden id="boardId" />
           <FormSubmit
