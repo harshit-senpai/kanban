3b8c1c6daf0467396c4caadbac72d05143640279
harshit Mishra <miananya2@gmail.com>
2024-08-20 15:12:01 +0530

    copyList action

---
+++ actions/copyList/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+import { List } from "@prisma/client";
+
+import { CopyList } from "./schema";
+
+import { ActionState } from "@/lib/createSafeAction";
+
+export type InputType = z.infer<typeof CopyList>;
+export type ReturnType = ActionState<InputType, List>;
