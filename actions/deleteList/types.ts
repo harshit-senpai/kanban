6290d903c58f00e660eebbdd9a23538d9754ea3d
harshit Mishra <miananya2@gmail.com>
2024-08-20 13:11:19 +0530

    deleteList action

---
+++ actions/deleteList/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+import { List } from "@prisma/client";
+
+import { DeleteList } from "./schema";
+
+import { ActionState } from "@/lib/createSafeAction";
+
+export type InputType = z.infer<typeof DeleteList>;
+export type ReturnType = ActionState<InputType, List>;
