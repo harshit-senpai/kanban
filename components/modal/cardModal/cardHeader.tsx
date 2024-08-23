eb62da775e649abe1356a24be25cadb532738f70
harshit Mishra <miananya2@gmail.com>
2024-08-23 13:29:38 +0530

    card detail

--- components/modal/cardModal/cardHeader.tsx
+++ components/modal/cardModal/cardHeader.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
+import { useParams } from "next/navigation";
+
+import { Layout } from "lucide-react";
+
+import { useQueryClient } from "@tanstack/react-query";
+
+import { CardWithList } from "@/types";
+
 import { FormInput } from "@/components/form/formInput";
 import { Skeleton } from "@/components/ui/skeleton";
-import { CardWithList } from "@/types";
-import { Layout } from "lucide-react";
-import { useState } from "react";
 
 interface HeaderProps {
   data: CardWithList;
@@ -12,17 +19,36 @@ interface HeaderProps {
 
 export const Header = ({ data }: HeaderProps) => {
   const [title, setTitle] = useState(data.title);
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const onblur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    console.log(title);
+  };
+
   return (
     <div className="flex items-center gap-x-3 mb-6 w-full">
       <Layout className="h-5 w-5 mt-1 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
+            ref={inputRef}
+            onBlur={onblur}
             id="title"
             defaultValue={title}
             className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
+        <p className="text-sm text-muted-foreground">
+          In List <span className="underline">{data.list.title}</span>
+        </p>
       </div>
     </div>
   );
