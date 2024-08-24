806e6804c6eccdbf142f3be666f85cb2e66c7966
harshit Mishra <miananya2@gmail.com>
2024-08-24 13:11:40 +0530

    deleteCard action

---
+++ actions/deletecard/index.ts
@@ -0,0 +1,44 @@
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+import { InputType, ReturnType } from "./types";
+import { DeleteCard } from "./schema";
+
+import { createSafeAction } from "@/lib/createSafeAction";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    card = await db.card.delete({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete",
+    };
+  }
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
---
+++ actions/deletecard/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+import { Card } from "@prisma/client";
+
+import { DeleteCard } from "./schema";
+
+import { ActionState } from "@/lib/createSafeAction";
+
+export type InputType = z.infer<typeof DeleteCard>;
+export type ReturnType = ActionState<InputType, Card>;
